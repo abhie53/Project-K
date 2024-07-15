1de4722fad6695422b60bd3014833a6ec3a2e8b2
darshh399 <sudarshanshinde399@gmail.com>
2024-07-15 20:05:01 +0530

    Implemented footer cardstyled and updated card home and card

--- src/components/NavigationComponents/Product/ProductData.js
+++ src/components/NavigationComponents/Product/ProductData.js
@@ -1,18 +1,21 @@
 import React from "react";
 import './ProductData.css';
 import ProductsData from "./ProductsData";
-import Card from "./Card";
-import ProductCard from './ProductCard'
+
+import { CardStyled } from "./Card.styles";
+
 const ProductData = () => {
     return(
         <div className="product-data-container">
-               {/* {
+               {
                 ProductsData.map((data) => {
                     return(
-                    <ProductCard data={data}/>
+                       <div className="product-container">
+                            <CardStyled data={data} />
+                       </div>
                     )
                 })
-               } */}
+               }
         </div>
     )
 };
--- src/components/PageComponent/Page.js
+++ src/components/PageComponent/Page.js
@@ -2,6 +2,7 @@ import Header from "../Header/Header";
 import Body from "../Body/Body";
 import Footer from "../Footer/Footer";
 
+
 const Page = () => {
 
     return (
