32a6ea93e5842df278b390213f83d05b5b36659e
aniket.jadhav <aniket.jadhav@qualitiasoft.com>
2024-07-21 00:50:44 +0530

    added styling for navlink and icons from contacts page

--- src/components/NavLink/NavLink.js
+++ src/components/NavLink/NavLink.js
@@ -1,17 +1,27 @@
-import './NavLink.css';
-import { Link } from 'react-router-dom';
+import "./NavLink.css";
+import { Link } from "react-router-dom";
 const NavLinks = () => {
-  return (
-    <nav className="nav-links">
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/contact'>Contact</Link></li>
-        <li><Link to='/services'>Services</Link></li>
-        <li><a href="#about">about</a></li>
-        <li><Link to='/products'>Products</Link></li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="nav-links">
+            <ul>
+                <li className="xd">
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/services">Services</Link>
+                </li>
+                <li>
+                    <a href="#about">about</a>
+                </li>
+                <li>
+                    <Link to="/products">Products</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default NavLinks;
